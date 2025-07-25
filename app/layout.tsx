c86aff6909da883b6f02e9635c35a6426804b6ca
Tom <smithtom6304@gmail.com>
2025-07-25 20:38:59 +0100

    Add vertical navbar for larger displays

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import localFont from "next/font/local";
 import "./globals.css";
-import {MenuBarHorizontal} from "@/app/ui/navigation/menu-bar";
+import {MenuBarHorizontal} from "@/app/ui/navigation/menu-bar-horizontal";
+import {useEffect, useState} from "react";
+import {MenuBarVertical} from "@/app/ui/navigation/menu-bar-vertical";
 
 const excalifont = localFont({
     src: [
@@ -18,17 +22,31 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    const isDesktopWidth = 800;
+    const [isDesktop, setDesktop] = useState(window.innerWidth > isDesktopWidth);
+
+    const updateMedia = () => {
+        setDesktop(window.innerWidth > isDesktopWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener("resize", updateMedia);
+        return () => window.removeEventListener("resize", updateMedia);
+    });
+
   return (
     <html lang="en">
       <body
-        id="root" className={`${excalifont.className} antialiased flex flex-col m-2 max-w-screen bg-blackboard-500`}
+        id="root" className={`${excalifont.className} antialiased flex ${isDesktop ? `flex-row` : `flex-col`} m-2 max-w-screen bg-blackboard-500`}
       >
+      { isDesktop ? <MenuBarVertical /> : null }
         <div className="overflow-y-auto flex-col">
             {children}
         </div>
-        <div className="sticky bottom-0 w-full">
-            <MenuBarHorizontal />
-        </div>
+      {!isDesktop ?
+          <div className="sticky bottom-0 w-full">
+            <MenuBarHorizontal/>
+        </div> : null}
       </body>
     </html>
   );
